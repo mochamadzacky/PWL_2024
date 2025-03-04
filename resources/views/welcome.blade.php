b38fe262148cc13aa9ba5ca6c077f9f6e54ee4eb
Mochamad Zacky Yudha A <muhammadzaky8630@gmail.com>
2025-03-05 00:16:36 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PWL 2024</title>
+        <head>
+            <body>
+                <h2>Pemrograman web lanjut 2024</h2>
+                <h2>Semangat Belajar Laravel 10</h2>
+            </body>
+        </html>
+        
+
+      
