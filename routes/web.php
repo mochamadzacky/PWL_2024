590a4ace8bbc21103ef9f0147df39588eaaad8d0
Mochamad Zacky Yudha A <muhammadzaky8630@gmail.com>
2025-03-06 11:39:24 +0700

    Praktikum 1 J2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+return 'Hello World';
+});
+
+
+Route::get('/world', function () {
+return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat datang';
+    });
+
+    Route::get('/about', function () {
+        return '2341760147 Zacky';
+        });
+
+        Route::get('/user/{Zacky}', function ($name) {
+            return 'Nama Saya '.$name;
+            });
+
+            Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function
+($id) {
+return 'Halaman artikel dengan id'. $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
